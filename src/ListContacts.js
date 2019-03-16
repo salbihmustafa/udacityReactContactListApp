51111bd91f7587a7ead18f99b1b1e09483fd9710
salbihmustafa <salbihdmustafa@gmail.com>
2019-03-15 19:16:44 -0500

    Add background image and show name and handle and add remove button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -9,8 +9,20 @@ class ListContacts extends Component {
         return (
             <ol className='contact-list'>
                 {this.props.contacts.map((contact) => ( 
-                    <li key={contact.id}> {/* Need to add key or else it will show error */}
-                        {contact.name} {/* This displays all the names from the array in App.js */}
+                    <li key={contact.id} className='contact-list-item'> {/* Need to add key or else it will show error */}
+                        <div 
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}
+                        ></div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            remove
+                        </button>
                     </li>
                 ))}
             </ol>
