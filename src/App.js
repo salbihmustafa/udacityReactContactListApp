001cf2f816cd70eab0f5b6afed2423bef5c0d096
salbihmustafa <salbihdmustafa@gmail.com>
2019-03-20 19:45:00 -0500

    went from stateless to state. Add state in ListContacts. Add updateQuery and input search box

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts' //Import our component from ListContacts.js
 
-//I copied this array of contacts from Udacity page.
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "@karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "@richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "@tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
 class App extends Component {
+
+  //Create State property and create contacts object. Copy the array inside the contacts object array
+  state = 
+  {
+    contacts: 
+    [
+        {
+          "id": "karen",
+          "name": "Karen Isgrigg",
+          "handle": "@karen_isgrigg",
+          "avatarURL": "http://localhost:5001/karen.jpg"
+        },
+        {
+          "id": "richard",
+          "name": "Richard Kalehoff",
+          "handle": "@richardkalehoff",
+          "avatarURL": "http://localhost:5001/richard.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "handle": "@tylermcginnis",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+    ]
+  }
+
+  //removeContact (passed item is previous state item)
+  removeContact = (contact) => {
+    //If you need to update previous state always use Functional setState
+    this.setState((currentState) => ({ 
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id //the id not equal to id passed in removeContact (contact)
+      })
+    }))
+  }
+
   render() {
     return (
       <div>
@@ -32,7 +47,10 @@ class App extends Component {
           Anything in between this div will show on the browser.
           We are showing component ListContacts with contacts above. 
         */}
-        <ListContacts contacts={contacts} /> {/* the contacts inside the bracket is the prop */}
+        <ListContacts 
+          contacts={this.state.contacts} //This is where contact prop is passed.
+          onDeleteContact={this.removeContact} //This is passing a function prop
+        /> 
       </div>
     );
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,69 @@
 //I created this file by right clicking src and pressing new file.
 //I am following a video.
 //This file is going to be responsible for listing all of the contacts.
-import React from 'react'
+import React, { Component} from 'react'
+import PropTypes from 'prop-types' //make sure to run command on cmd "yarn add prop-types"
 
-function ListContacts (props)
+class ListContacts extends Component 
 {
-    return (
-        <ol className='contact-list'> {/* contact-list is from CSS */}
-            {props.contacts.map((contact) => ( 
-                <li key={contact.id} className='contact-list-item'> {/* Need to add key or else it will show error */}
-                    <div 
-                        className='contact-avatar' 
-                        style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}
-                    ></div>{/* contact-avatar is coming from CSS */}
-                    <div className='contact-details'> {/* contact-details is coming from CSS */}
-                        <p>{contact.name}</p>
-                        <p>{contact.handle}</p>
-                    </div>
-                    <button className='contact-remove'>
-                        remove {/* Button to remove contacts. contact-remove is coming from CSS*/}
-                    </button>
-                </li>
-            ))}
-        </ol>
-    )
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+    state = {
+        query: '' //Initalized and is blank
+    }
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim() //This is where query gets reset from users values
+        }))
+    }
+
+    render() 
+    {
+        return(
+            <div className='list-contacts'>
+                {JSON.stringify(this.state)} {/* This is the string beneath the search input */} 
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts' //I assume this is from a pre-built CSS
+                        type='text' //Type of value going into this search box
+                        placeholder='Search Contacts' //Place holder when no one types anything in the search box
+                        value={this.state.query} //The value is being updated onChange and is changing the state in updateQuery
+                        onChange={(event) => this.updateQuery(event.target.value)} //This event happens when user starts typing in the search field then "event.target.value" will capture keystrokes and pass the value to updateQuery
+                    />
+                </div>
+                <ol className='contact-list'> {/* contact-list is from CSS */}
+                    {this.props.contacts.map((contact) => ( 
+                        <li key={contact.id} className='contact-list-item'> {/* Need to add key or else it will show error */}
+                            <div 
+                                className='contact-avatar' 
+                                style={{
+                                    backgroundImage: `url(${contact.avatarURL})`
+                                }}
+                            ></div>{/* contact-avatar is coming from CSS */}
+                            <div className='contact-details'> {/* contact-details is coming from CSS */}
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button 
+                                onClick={() => this.props.onDeleteContact(contact)} //Added onclick function and passing function prop
+                                className='contact-remove'>
+                                remove {/* Button to remove contacts. contact-remove is coming from CSS*/}
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
+}
+
+//These are assigned Prop types
+//.array is an array type. .func is a function type.
+ListContacts.PropTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
 }
 
 //Whenever creating a component make sure to default the class
